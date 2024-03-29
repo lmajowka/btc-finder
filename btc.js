b52dd13de92f73b8e2191b540a5a2e5aedeae20b
Leonardo Majowka <lmajowka@users.noreply.github.com>
2024-03-29 13:29:32 +0300

    first and only commit

---
+++ btc.js
@@ -0,0 +1,35 @@
+﻿var CoinKey = require('coinkey')
+
+const min = 0x2126875fd00000000
+const max = 0x3ffffffffffffffff
+
+const wallets = ['13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so']
+
+let key = BigInt(min)
+
+console.log(key)
+
+while(true){
+
+    key = key + BigInt(parseInt(1))
+    pkey = key.toString(16)
+    while (pkey.length < 64){
+        pkey = '0' + pkey
+    }
+
+    public = generatePublic(pkey)
+    console.log(pkey)
+    console.log(public)
+    for (i=0;i<wallets.length;i++){
+        if (public == wallets[i]){
+            throw 'ACHEI!!!! 🎉🎉🎉🎉🎉'
+        }
+    }
+}
+
+
+function generatePublic(privateKey){
+    _key = new CoinKey(new Buffer(privateKey, 'hex'))
+    _key.compressed = true
+    return _key.publicAddress
+}
