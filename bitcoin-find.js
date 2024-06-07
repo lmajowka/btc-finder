9f0d280b652d61e13bf85c9c6fffc92a7fb7de77
luancyrne <luancyrne@outlook.com.br>
2024-06-07 16:20:17 -0300

    feat: fix stop ctrl + c & npm start

--- bitcoin-find.js
+++ bitcoin-find.js
@@ -1,90 +1,90 @@
 import CoinKey from 'coinkey';
 import walletsArray from './wallets.js';
-import chalk from 'chalk'
+import chalk from 'chalk';
 import fs from 'fs';
+
 const walletsSet = new Set(walletsArray);
 
-async function encontrarBitcoins(key, min, max, shouldStop){
-
+async function encontrarBitcoins(key, min, max, shouldStop) {
     let segundos = 0;
-    let pkey = 0;
-    const um = BigInt(1);
-    const startTime = Date.now()
+    const startTime = Date.now();
 
-    let zeroes = new Array(65).fill('');
-    for (let i=1;i<64;i++){
-        zeroes[i] = '0'.repeat(64 - i);
-    }
+    const zeroes = Array.from({ length: 65 }, (_, i) => '0'.repeat(64 - i));
 
-    console.log('Buscando Bitcoins...')
+    console.log('Buscando Bitcoins...');
 
     const executeLoop = async () => {
-    while(!shouldStop()){
-    
-        key++; 
-        pkey = key.toString(16)
-        pkey = `${zeroes[pkey.length]}${pkey}`;
-    
-    
-        if (Date.now() - startTime > segundos){
-            segundos += 1000
-            console.log(segundos/1000);
-            if (segundos % 10000 == 0){
-              const tempo = (Date.now() - startTime) / 1000;
-              console.clear();
-              console.log('Resumo: ')
-              console.log('Velocidade:', (Number(key) - Number(min))/ tempo, ' chaves por segundo')
-              console.log('Chaves buscadas: ', (key - min).toLocaleString('pt-BR'));    
-              console.log('Ultima chave tentada: ',pkey )
+        while (!shouldStop()) {
+            key++;
+            let pkey = key.toString(16);
+            pkey = `${zeroes[pkey.length]}${pkey}`;
 
-              const filePath = 'Ultima_chave.txt';  // File path to write to
-              const content =`Ultima chave tentada: ${pkey}`
-              try {
-                fs.writeFileSync(filePath, content, 'utf8');
-              } catch (err) {
-                console.error('Error writing to file:', err);
-              }
-            }
-        }
-    
-        let publicKey = generatePublic(pkey)
-        if (walletsSet.has(publicKey)){
-            const tempo = (Date.now() - startTime)/1000
-            console.log('Velocidade:', (Number(key) - Number(min))/ tempo, ' chaves por segundo')
-            console.log('Tempo:', tempo, ' segundos');
-            console.log('Private key:', chalk.green(pkey))
-            console.log('WIF:', chalk.green(generateWIF(pkey)))
+            if (Date.now() - startTime > segundos) {
+                segundos += 1000;
+                console.log(segundos / 1000);
+                if (segundos % 10000 === 0) {
+                    const tempo = (Date.now() - startTime) / 1000;
+                    console.clear();
+                    console.log('Resumo: ');
+                    console.log('Velocidade:', (Number(key) - Number(min)) / tempo, ' chaves por segundo');
+                    console.log('Chaves buscadas: ', (key - min).toLocaleString('pt-BR'));
+                    console.log('Ultima chave tentada: ', pkey);
 
-            const filePath = 'keys.txt';
-            const lineToAppend = `Private key: ${pkey}, WIF: ${generateWIF(pkey)}\n`;
-
-            try {
-                fs.appendFileSync(filePath, lineToAppend);
-                console.log('Chave escrita no arquivo com sucesso.');
-            } catch (err) {
-                console.error('Erro ao escrever chave em arquivo:', err);
+                    const filePath = 'Ultima_chave.txt';
+                    const content = `Ultima chave tentada: ${pkey}`;
+                    try {
+                        fs.writeFileSync(filePath, content, 'utf8');
+                    } catch (err) {
+                        console.error('Erro ao escrever no arquivo:', err);
+                    }
+                }
             }
 
-            throw 'ACHEI!!!! 🎉🎉🎉🎉🎉'
+            let publicKey = generatePublic(pkey);
+            if (walletsSet.has(publicKey)) {
+                const tempo = (Date.now() - startTime) / 1000;
+                console.log('Velocidade:', (Number(key) - Number(min)) / tempo, ' chaves por segundo');
+                console.log('Tempo:', tempo, ' segundos');
+                console.log('Private key:', chalk.green(pkey));
+                console.log('WIF:', chalk.green(generateWIF(pkey)));
+
+                const filePath = 'keys.txt';
+                const lineToAppend = `Private key: ${pkey}, WIF: ${generateWIF(pkey)}\n`;
+
+                try {
+                    fs.appendFileSync(filePath, lineToAppend);
+                    console.log('Chave escrita no arquivo com sucesso.');
+                } catch (err) {
+                    console.error('Erro ao escrever chave em arquivo:', err);
+                }
+
+                throw 'ACHEI!!!! 🎉🎉🎉🎉🎉';
+            }
+
+            await new Promise(resolve => setImmediate(resolve));
         }
-        
+    };
+
+    try {
+        await executeLoop();
+    } catch (err) {
+        if (err !== 'ACHEI!!!! 🎉🎉🎉🎉🎉') {
+            console.error('Erro inesperado:', err);
+        }
     }
-    await new Promise(resolve => setImmediate(resolve));
-    }
-    await executeLoop();
+
+    console.log('Processo interrompido ou concluído.');
 }
 
-function generatePublic(privateKey){
-    let _key = new CoinKey(new Buffer(privateKey, 'hex'))
-    _key.compressed = true
-    return _key.publicAddress
+function generatePublic(privateKey) {
+    let _key = new CoinKey(Buffer.from(privateKey, 'hex'));
+    _key.compressed = true;
+    return _key.publicAddress;
 }
 
-function generateWIF(privateKey){
-    let _key = new CoinKey(new Buffer(privateKey, 'hex'))
-    return _key.privateWif
+function generateWIF(privateKey) {
+    let _key = new CoinKey(Buffer.from(privateKey, 'hex'));
+    return _key.privateWif;
 }
-
-
 
 export default encontrarBitcoins;
