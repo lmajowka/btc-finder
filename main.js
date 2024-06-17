e131acd563a44a189dc2f250f380e85f4558d5c3
jack970 <italocod@hotmail.com>
2024-06-16 22:51:10 -0300

    [refactor] import utils to main

--- main.js
+++ main.js
@@ -1,91 +1,69 @@
-﻿import ranges from './ranges.js'
+﻿import chalk from 'chalk'
 import encontrarBitcoins from './bitcoin-find.js'
-import readline from 'readline'
-import chalk from 'chalk'
+import {
+    fazerPergunta, rl,
+    escolherCarteira,
+    escolherMinimo,
+    escolherPorcentagem
+} from './utils/index.js'
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let shouldStop = false;
-
-
-
-let key = 0;
-let min, max = 0;
-
-console.clear();
-
-console.log("\x1b[38;2;250;128;114m" + "╔════════════════════════════════════════════════════════╗\n" +
-            "║" + "\x1b[0m" + "\x1b[36m" + "   ____ _____ ____   _____ ___ _   _ ____  _____ ____   " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
-            "║" + "\x1b[0m" + "\x1b[36m" + "  | __ )_   _/ ___| |  ___|_ _| \\ | |  _ \\| ____|  _ \\  " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
-            "║" + "\x1b[0m" + "\x1b[36m" + "  |  _ \\ | || |     | |_   | ||  \\| | | | |  _| | |_) | " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
-            "║" + "\x1b[0m" + "\x1b[36m" + "  | |_) || || |___  |  _|  | || |\\  | |_| | |___|  _ <  " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
-            "║" + "\x1b[0m" + "\x1b[36m" + "  |____/ |_| \\____| |_|   |___|_| \\_|____/|_____|_| \\_\\ " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
-            "║" + "\x1b[0m" + "\x1b[36m" + "                                                        " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
-            "╚══════════════════════\x1b[32m" + "Investidor Internacional - v0.5" + "\x1b[0m\x1b[38;2;250;128;114m═══╝" + "\x1b[0m");
-
-rl.question(`Escolha uma carteira puzzle( ${chalk.cyan(1)} - ${chalk.cyan(160)}): `, (answer) => {
-    
-    if (parseInt(answer) < 1 || parseInt(answer) > 160) {
-        console.log(chalk.bgRed('Erro: voce precisa escolher um numero entre 1 e 160'))
-    }
-
-    min = ranges[answer-1].min
-    max = ranges[answer-1].max
-    console.log('Carteira escolhida: ', chalk.cyan(answer), ' Min: ', chalk.yellow(min), ' Max: ', chalk.yellow(max) )
-    console.log('Numero possivel de chaves:',  chalk.yellow(parseInt(BigInt(max) - BigInt(min)).toLocaleString('pt-BR')))
-    let status = ''
-    if (ranges[answer-1].status == 1){
-        status =  chalk.red('Encontrada')
-    } else  {
-        status =  chalk.green('Nao Encontrada')
-    }
-
-    console.log('Status: ', status)
-    key = BigInt(min)
-    
-    rl.question(`Escolha uma opcao (${chalk.cyan(1)} - Comecar do inicio, ${chalk.cyan(2)} - Escolher uma porcentagem, ${chalk.cyan(3)} - Escolher minimo): `, (answer2) => {
-        if (answer2 == '2'){
-            rl.question('Escolha um numero entre 0 e 1: ', (answer3) => {
-                if (parseFloat(answer3) > 1 || parseFloat(answer3) < 0) {
-                    console.log(chalk.bgRed('Erro: voce precisa escolher um numero entre 0 e 1'))
-                    throw 'Numero invalido'
-                }
+function titulo() {
+    console.log("\x1b[38;2;250;128;114m" + "╔════════════════════════════════════════════════════════╗\n" +
+        "║" + "\x1b[0m" + "\x1b[36m" + "   ____ _____ ____   _____ ___ _   _ ____  _____ ____   " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+        "║" + "\x1b[0m" + "\x1b[36m" + "  | __ )_   _/ ___| |  ___|_ _| \\ | |  _ \\| ____|  _ \\  " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+        "║" + "\x1b[0m" + "\x1b[36m" + "  |  _ \\ | || |     | |_   | ||  \\| | | | |  _| | |_) | " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+        "║" + "\x1b[0m" + "\x1b[36m" + "  | |_) || || |___  |  _|  | || |\\  | |_| | |___|  _ <  " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+        "║" + "\x1b[0m" + "\x1b[36m" + "  |____/ |_| \\____| |_|   |___|_| \\_|____/|_____|_| \\_\\ " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+        "║" + "\x1b[0m" + "\x1b[36m" + "                                                        " + "\x1b[0m" + "\x1b[38;2;250;128;114m" + "║\n" +
+        "╚══════════════════════\x1b[32m" + "Investidor Internacional - v0.6" + "\x1b[0m\x1b[38;2;250;128;114m═══╝" + "\x1b[0m");
+}
 
 
-                const range = BigInt(max) - BigInt(min);
-                const percentualRange = range * BigInt(Math.floor(parseFloat(answer3) * 1e18)) / BigInt(1e18);
-                min = BigInt(min) + BigInt(percentualRange);
-                console.log('Comecando em: ', chalk.yellow('0x'+min.toString(16)));
-                key = BigInt(min)
-                encontrarBitcoins(key, min, max, () => shouldStop)
-                rl.close();
-            });
-        } else if (answer2 == '3'){
-            rl.question('Entre o minimo: ', (answer3) => {
-                min = BigInt(answer3)
-                key = BigInt(min)
-                encontrarBitcoins(key, min, max, () => shouldStop)
-                rl.close();
-            });
-        } else {
-            min = BigInt(min)
-            encontrarBitcoins(key, min, max, () => shouldStop)
+async function menu() {
+    let [min, max, key] = await escolherCarteira(`Escolha uma carteira puzzle( ${chalk.cyan(1)} - ${chalk.cyan(160)}): `)
+    const answer = await fazerPergunta(`Escolha uma opcao (${chalk.cyan(1)} - Comecar do inicio, ${chalk.cyan(2)} - Escolher uma porcentagem, ${chalk.cyan(3)} - Escolher minimo): `)
+
+    switch (answer) {
+        case '2':
+            [min, max, key] = await escolherPorcentagem(min, max)
+            rl.close()
+
+            key = min;
+
+            encontrarBitcoins(key, min, max)
+            break
+        case '3':
+            [min, key] = await escolherMinimo(min)
             rl.close();
-        }
-    })
-});
+
+            encontrarBitcoins(key, min, max)
+            break
+        default:
+            min = BigInt(min)
+            rl.close();
+
+            encontrarBitcoins(key, min, max)
+            break
+    }
+}
+
+function main() {
+    console.clear()
+    titulo()
+    menu()
+}
+
+main()
+
 
 rl.on('SIGINT', () => {
-    shouldStop = true;
+    console.log("\nFechando Programa!")
     rl.close();
     process.exit();
 });
 
-process.on('SIGINT', () => {
-    shouldStop = true;
-    rl.close();
-    process.exit();
-});
+// process.on('SIGINT', () => {
+//     console.log("\nFechando Programa!")
+//     rl.close();
+//     process.exit();
+// });
